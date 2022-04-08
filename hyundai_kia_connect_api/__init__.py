adac716075bb15efe227463b3dbe1910e311af76
Eric <me@bostrot.com>
2022-04-09 01:05:44 +0200

    Update __init__.py

--- hyundai_kia_connect_api/__init__.py
+++ hyundai_kia_connect_api/__init__.py
@@ -1,10 +1,10 @@
 """Top-level package for Hyundai / Kia Connect."""
 
 from .ApiImpl import ApiImpl, ClimateRequestOptions
-from .HyundaiBlueLinkAPIUSA import HyundaiBlueLinkAPIUSA
+# from .HyundaiBlueLinkAPIUSA import HyundaiBlueLinkAPIUSA
 from .KiaUvoApiCA import KiaUvoApiCA
 from .KiaUvoApiEU import KiaUvoApiEU
-from .KiaUvoAPIUSA import KiaUvoAPIUSA
+#from .KiaUvoAPIUSA import KiaUvoAPIUSA
 
 from .Token import Token
 from .Vehicle import Vehicle
